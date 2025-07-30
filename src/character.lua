2006879c99203c2af7683d03d81cc9415f9b2494
Keaton Tate <43621442+keatontate@users.noreply.github.com>
2025-07-30 17:08:15 -0600

    Added basic player movement

--- src/character.lua
+++ src/character.lua
@@ -1,8 +1,16 @@
-character={
+pl={
  x=63,
- y=63
+ y=63,
+ spd=1
 }
 
-function draw_character()
- 
+function update_pl()
+ if (btn(0)) pl.x -= pl.spd
+ if (btn(1)) pl.x += pl.spd 
+ if (btn(2)) pl.y -= pl.spd 
+ if (btn(3)) pl.y += pl.spd
+end
+
+function draw_pl()
+ spr(1,pl.x,pl.y,2,2)
 end
--- src/game-loop.lua
+++ src/game-loop.lua
@@ -2,11 +2,12 @@ function _init()
 end
 
 function _update()
+ update_pl()
 end
 
 function _draw()
  cls()
- draw_character()
+ draw_pl()
  -- circfill(64,64,5,7)
  -- if btn(2) then
  --  circfill(64,64,10,6)
