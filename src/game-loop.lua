bce250529952126e2edf788b210a682f23fc510d
jamfrancis <jamestelfordfrancis@gmail.com>
2025-07-31 09:50:38 -0500

    Fixed commit issue

--- src/game-loop.lua
+++ src/game-loop.lua
@@ -5,28 +5,13 @@ function _init()
 end
 
 function _update()
-<<<<<<< Updated upstream
   update_pl()
-end
-
-function _draw()
-  cls()
-  draw_pl()
-  -- circfill(64,64,5,7)
-  -- if btn(2) then
-  --  circfill(64,64,10,6)
-  -- end
-=======
- update_pl()
-
-
 end
 
 function _draw()
  cls()
  draw_pl()
+
  -- temp floor
  map(0, 12, 0, 91, 16, 1)
-
->>>>>>> Stashed changes
 end
