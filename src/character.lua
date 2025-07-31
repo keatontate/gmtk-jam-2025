763d90d970983db41fc7613252323adac96ae012
jamfrancis <jamestelfordfrancis@gmail.com>
2025-07-31 09:41:56 -0500

    Add gravity and jumping to character movement
    Implemented basic gravity and jumping mechanics for the player character. Added collision detection with a temporary floor using tile 48. Updated the game loop to generate a floor and render it with the map function.

--- src/character.lua
+++ src/character.lua
@@ -1,14 +1,64 @@
 pl={
+<<<<<<< Updated upstream
   x=63,
   y=63,
   spd=1
+=======
+ x=63,
+ y=63,
+ velo = 0,
+ grounded = false,
+ spd=1
+>>>>>>> Stashed changes
 }
 
+gravity = 0.3
+jump = -2.5
+
+-- collision checker
+function is_solid(x, y)
+    local tile_x = flr(x / 8)
+    local tile_y = flr(y / 8)
+    local tile = mget(tile_x, tile_y)
+
+    -- temp floor
+    return tile == 48
+end
+
 function update_pl()
+<<<<<<< Updated upstream
   if (btn(0)) pl.x -= pl.spd
   if (btn(1)) pl.x += pl.spd 
   if (btn(2)) pl.y -= pl.spd 
   if (btn(3)) pl.y += pl.spd
+=======
+ if (btn(0)) pl.x -= pl.spd
+ if (btn(1)) pl.x += pl.spd 
+
+ -- jump
+ if (btn(2) and pl.grounded) then
+    pl.grounded = false
+    pl.velo = jump
+ end
+ 
+
+ -- enables gravity
+ if not pl.grounded then
+    pl.velo += gravity
+ end
+
+ local new_y = pl.y + pl.velo
+
+ -- temp floor
+    if is_solid(pl.x + 8, new_y + 15) then
+        pl.y = flr((new_y + 15) / 8) * 8 - 16
+        pl.velo = 0
+        pl.grounded = true
+    else
+        pl.y = new_y
+        pl.grounded = false
+    end
+>>>>>>> Stashed changes
 end
 
 function draw_pl()
--- src/game-loop.lua
+++ src/game-loop.lua
@@ -1,7 +1,11 @@
 function _init()
+    for x = 0, 15 do
+        mset(x, 12, 48)
+    end
 end
 
 function _update()
+<<<<<<< Updated upstream
   update_pl()
 end
 
@@ -12,4 +16,17 @@ function _draw()
   -- if btn(2) then
   --  circfill(64,64,10,6)
   -- end
+=======
+ update_pl()
+
+
+end
+
+function _draw()
+ cls()
+ draw_pl()
+ -- temp floor
+ map(0, 12, 0, 91, 16, 1)
+
+>>>>>>> Stashed changes
 end
