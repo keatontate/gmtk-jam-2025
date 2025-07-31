857790f1c69fcef332d619bd62998e973a0db1b8
john9francis <john9francis@gmail.com>
2025-07-31 18:09:52 -0400

    Got some 4 directional collision boxes going. It's not the most polished ever, but it works

--- src/character.lua
+++ src/character.lua
@@ -4,38 +4,60 @@ pl={
   spd=1,
   velo = 0,
   grounded = false,
-  width = 16,
-  height = 16
+  w = 16,
+  h = 16
 }
 
 gravity = 0.3
 jump = -2.5
 
--- collision checker
+-- collision checkers
 function is_solid(x, y)
-    local tile_x = flr(x / 8)
-    local tile_y = flr(y / 8)
-    local tile = mget(tile_x, tile_y)
+  local tile_x = flr(x / 8)
+  local tile_y = flr(y / 8)
+  local tile = mget(tile_x, tile_y)
 
-    -- check if the flag is 0
-    return fget(tile, 0)
+  -- check if the flag is 0
+  return fget(tile, 0)
 end
 
 function update_pl()
 
   -- check collissions
+  can_move_down = true
+  can_move_left = true
+  can_move_right = true
+  can_move_up = true
+
+  -- buffer gives the player a little
+  -- wiggle room and makes the game
+  -- feel less choppy
+  buffer = 2
+
+  if is_solid(pl.x + pl.w/2, pl.y + pl.h) then
+    can_move_down = false
+  end
+  if is_solid(pl.x, pl.y + pl.h - buffer) then
+    can_move_left = false
+  end
+  if is_solid(pl.x + pl.w, pl.y + pl.h - buffer) then
+    can_move_right = false
+  end
+  if is_solid(pl.x + pl.w/2, pl.y - buffer) then
+    can_move_up = false
+  end
 
 
-  if (btn(0)) pl.x -= pl.spd
-  if (btn(1)) pl.x += pl.spd 
+  -- movement logic
+  if (btn(0)) and can_move_left pl.x -= pl.spd
+  if (btn(1)) and can_move_right pl.x += pl.spd 
 
   -- jump
-  if (btn(2) and pl.grounded) then
+  if (btn(2) and pl.grounded and can_move_up) then
     pl.grounded = false
     pl.velo = jump
   end
  
-
   -- enables gravity
   if not pl.grounded then
     pl.velo += gravity
@@ -43,14 +65,14 @@ function update_pl()
 
   local new_y = pl.y + pl.velo
 
-  -- check if floor is solid
-  if is_solid(pl.x + pl.width/2, new_y + pl.height) then
-    pl.y = flr((new_y + pl.height) / 8) * 8 - 16
-    pl.velo = 0
-    pl.grounded = true
-  else
+  -- update player y based on vel
+  if can_move_down then
     pl.y = new_y
     pl.grounded = false
+  else
+    pl.y = flr((new_y + pl.h) / 8) * 8 - 16
+    pl.velo = 0
+    pl.grounded = true
   end
 
 end
@@ -59,4 +81,5 @@ function draw_pl()
   -- note: player's actual position 
   -- is at top left corner of sprite
   spr(1,pl.x,pl.y,2,2)
+  print(pl.velo)
 end
