ee47e0d80c8237a8dc14b466925675c07e77cd46
john9francis <john9francis@gmail.com>
2025-07-30 23:12:55 -0400

    Fixed tabs

--- src/character.lua
+++ src/character.lua
@@ -1,16 +1,16 @@
 pl={
- x=63,
- y=63,
- spd=1
+  x=63,
+  y=63,
+  spd=1
 }
 
 function update_pl()
- if (btn(0)) pl.x -= pl.spd
- if (btn(1)) pl.x += pl.spd 
- if (btn(2)) pl.y -= pl.spd 
- if (btn(3)) pl.y += pl.spd
+  if (btn(0)) pl.x -= pl.spd
+  if (btn(1)) pl.x += pl.spd 
+  if (btn(2)) pl.y -= pl.spd 
+  if (btn(3)) pl.y += pl.spd
 end
 
 function draw_pl()
- spr(1,pl.x,pl.y,2,2)
+  spr(1,pl.x,pl.y,2,2)
 end
--- src/game-loop.lua
+++ src/game-loop.lua
@@ -2,14 +2,14 @@ function _init()
 end
 
 function _update()
- update_pl()
+  update_pl()
 end
 
 function _draw()
- cls()
- draw_pl()
- -- circfill(64,64,5,7)
- -- if btn(2) then
- --  circfill(64,64,10,6)
- -- end
+  cls()
+  draw_pl()
+  -- circfill(64,64,5,7)
+  -- if btn(2) then
+  --  circfill(64,64,10,6)
+  -- end
 end
